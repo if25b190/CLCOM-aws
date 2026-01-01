a0b7fc06ec12a2c86968b2d6e7a81b277bd9937f
Martin Six <if25b050@technikum-wien.at>
2026-01-01 15:26:50 +0100

    Gitlab + Ldap Konfiguriert

--- ldap-server/setup_ldap_server.sh
+++ ldap-server/setup_ldap_server.sh
@@ -4,6 +4,7 @@ mkdir -p /srv/ldap
 export LDAP_HOME=/srv/ldap
 
 curl https://raw.githubusercontent.com/if25b190/CLCOM-aws/refs/heads/main/ldap-server/ldap.docker-compose.yml > $LDAP_HOME/ldap.docker-compose.yml
-curl https://raw.githubusercontent.com/if25b190/CLCOM-aws/refs/heads/main/ldap-server/gitlab-admin.ldif > $LDAP_HOME/gitlab-admin.ldif
+curl https://raw.githubusercontent.com/if25b190/CLCOM-aws/refs/heads/main/ldap-server/ldap-groups.ldif > $LDAP_HOME/ldap-groups.ldif
+curl https://raw.githubusercontent.com/if25b190/CLCOM-aws/refs/heads/main/ldap-server/ldap-users.ldif > $LDAP_HOME/ldap-users.ldif
 cd $LDAP_HOME
 docker compose -f ./ldap.docker-compose.yml up -d
